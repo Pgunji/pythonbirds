09e9669e5a805d3aaebc8c8ead1940050bbadf1c
gunji <pgunji@gmail.com>
2020-06-03 17:20:32 -0300

    criada atributos nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,21 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=49):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Ola!'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
-    print (id(p))
+    print (p.nome)
+    p.nome = 'Paulo'
+    print(p.nome)
+    print(p.idade)
+
+
 
 
 
